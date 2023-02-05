7bfc3102fbe1bef2bffbe6c99e23c3e2cc1cd4c0
Sohrab Alam <sohrab.alam86@gmail.com>
2023-02-05 11:59:58 +0530

    Resilience & Circuit Breaker Pattern

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
+@RequestMapping("resilience4j")
 public class CircuitBreakerController {
 
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
